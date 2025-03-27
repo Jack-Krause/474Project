ff2ded7422907042871e64029b8a8faff68227a0
Jack Krause <jmkrause@iastate.edu>
2025-03-27 17:55:22 -0500

    change to better NOAA dataset

--- get_data/retrieve_data.py
+++ get_data/retrieve_data.py
@@ -2,12 +2,12 @@ import os
 import requests
 
 # base_url = "https://www.ncei.noaa.gov/cdo-web/api/v2/{endpoint}"
-base_url = "https://www.ncei.noaa.gov/cdo-web/api/v2/datasets/LCD"
+base_url = "https://www.ncei.noaa.gov/cdo-web/api/v2/datasets/GSOM"
 token = os.environ.get("NOAA_JACK")
 headers = {"token": token}
 
-def download_dataset(url=base_url, save_dir='data', filename=base_url):
-    print("downloading dataset from NOAA")
+def send_request(url=base_url, save_dir='data', filename=base_url):
+    print("sending call: " + url)
     response = requests.get(url, headers=headers)
     print(response.text)
 
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import get_data.retrieve_data as get_data
 
-
+data_categories_url = "https://www.ncei.noaa.gov/cdo-web/api/v2/datacategories/?datasetid=GSOM"
 # output the api request from NOAA
-get_data.download_dataset()
+get_data.send_request()
+get_data.send_request(url=data_categories_url)
+
 

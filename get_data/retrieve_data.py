af64e10e3cb8dfa58100a1623946a4abe0ad7f97
Jack Krause <jmkrause@iastate.edu>
2025-03-27 19:02:26 -0500

    makes call for data

--- get_data/retrieve_data.py
+++ get_data/retrieve_data.py
@@ -1,3 +1,4 @@
+import csv
 import os
 import requests
 
@@ -11,4 +12,33 @@ def send_request(url=base_url, save_dir='data', filename=base_url):
     response = requests.get(url, headers=headers)
     print(response.text)
 
+
+def get_dataset(url=base_url, save_dir='data', filename=base_url):
+    print("get dataset: " + url)
+    stations = csv_to_str(os.getcwd() + r'\data\arizona_maricopa_stations.csv')
+    print("stations:\n", stations)
+
+    params = {
+        "startdate": "2017-01-01",
+        "enddate": "2021-01-01",
+        "stationid": "GHCND:USC00020060"
+    }
+
+    response = requests.get(url, headers=headers, params=params)
+    print(response.text)
+
+
+def csv_to_str(path, save=False):
+    print("path of csv file: " + path)
+    data = ""
+    with open(path, 'r') as f:
+        reader = csv.reader(f)
+        i = 0
+        for row in reader:
+            if i > 0:
+                data += row[0]
+                data += ' & '
+                break
+
+    return data
 
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import get_data.retrieve_data as get_data
 
+get_data_url = "https://www.ncei.noaa.gov/cdo-web/api/v2/data?datasetid=GSOM"
 data_categories_url = "https://www.ncei.noaa.gov/cdo-web/api/v2/datacategories/?datasetid=GSOM"
 # output the api request from NOAA
-get_data.send_request()
-get_data.send_request(url=data_categories_url)
+# get_data.send_request()
+# get_data.send_request(url=data_categories_url)
+get_data.get_dataset(url=get_data_url)
 
 

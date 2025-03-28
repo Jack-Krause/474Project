b76f3cf20c4faf2a212d68f343da15185aef9b7e
Jack Krause <jmkrause@iastate.edu>
2025-03-28 14:59:34 -0500

    csv downloaded

--- get_data/retrieve_data.py
+++ get_data/retrieve_data.py
@@ -39,3 +39,21 @@ def csv_to_str(path, save=False):
 
     return data
 
+
+def download_csv_data(url, save_path):
+    response = requests.get(url)
+    response.raise_for_status()
+
+    with open(save_path, 'wb') as f:
+        f.write(response.content)
+
+
+
+
+
+
+
+
+
+
+

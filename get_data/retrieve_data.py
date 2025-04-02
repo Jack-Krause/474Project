f75194999816f3537109c627687a5f85e7916f50
Jack Krause <jmkrause@iastate.edu>
2025-04-02 18:44:30 -0500

    csv parsing

--- get_data/retrieve_data.py
+++ get_data/retrieve_data.py
@@ -1,59 +1,38 @@
 import csv
 import os
 import requests
-
-# base_url = "https://www.ncei.noaa.gov/cdo-web/api/v2/{endpoint}"
-base_url = "https://www.ncei.noaa.gov/cdo-web/api/v2/datasets/GSOM"
-token = os.environ.get("NOAA_JACK")
-headers = {"token": token}
-
-def send_request(url=base_url, save_dir='data', filename=base_url):
-    print("sending call: " + url)
-    response = requests.get(url, headers=headers)
-    print(response.text)
+import pandas as pd
+import json
 
 
-def get_dataset(url=base_url, save_dir='data', filename=base_url):
-    print("get dataset: " + url)
-    # station_ids = csv_to_str(os.getcwd() + r'\data\arizona_maricopa_stations.csv')
-    station_ids = ["GHCND:USW00003184"]  # phoenix deer valley muni airport
-    print(station_ids)
-
-    params = [
-        ("startdate", "2017-01-01"),
-        ("enddate", "2021-01-01"),
-        ("enddate", "2021-01-01")
-    ]
-
-    response = requests.get(url, headers=headers, params=params)
-    print(response.text)
-
-
-def csv_to_str(path, save=False):
+def parse_csv(path, save_headers=False, chosen_features=None, save=False):
     print("path of csv file: " + path)
-    data = []
-    with open(path, 'r') as f:
-        reader = csv.reader(f)
-        for row in reader:
-            data.append(row[0])
 
-    return data
+    root_dir = os.environ.get("ROOT_DIR")
+    parsed_data_dir = os.path.join(root_dir, "parsed_data")
+    if save_headers:
+        save_headers_json(path, parsed_data_dir)
+
+    with open(os.path.join(parsed_data_dir, "headers.json")) as headers_json:
+        headers_obj = json.load(headers_json)
+        data = pd.read_csv(path, dtype=headers_obj)
+        header_row = data.columns.tolist()
+        return data, header_row
+
+
+
+def save_headers_json(path, save_path):
+    os.makedirs(save_path, exist_ok=True)
+    data = pd.read_csv(path, low_memory=False)
+    dtypes_dict = data.dtypes.apply(lambda dt: dt.name).to_dict()
+
+    with open(os.path.join(save_path, "headers.json"), "w") as f:
+        json.dump(dtypes_dict, f, indent=4)
 
 
 def download_csv_data(url, save_path):
     response = requests.get(url)
     response.raise_for_status()
 
-    with open(save_path, 'wb') as f:
+    with open(save_path, "wb") as f:
         f.write(response.content)
-
-
-
-
-
-
-
-
-
-
-
--- main.py
+++ main.py
@@ -2,42 +2,20 @@ import get_data.retrieve_data as get_data
 import ml_training.process_data as process_data
 import os
 
-base_url = "https://www.ncei.noaa.gov/data/global-summary-of-the-month/access/"
-station_id = "USW00003184"
-# station_id = "GHCND:USW00003184"
 
-data_directory = "data"
-os.makedirs(data_directory, exist_ok=True)
+root_dir = os.environ.get("ROOT_DIR")
+dataloc = os.path.join(root_dir, "data", "Pavement.csv")
+parsed_data_dir = os.path.join(root_dir, "parsed_data")
 
-file_url = f"{base_url}{station_id}.csv"
-save_path = os.path.join(data_directory, f"{station_id}.csv")
+if not os.path.isdir(parsed_data_dir):
+    os.mkdir(parsed_data_dir)
 
-try:
-    get_data.download_csv_data(file_url, save_path)
-except Exception as e:
-    print(f"error downloading data: {e}")
+if not os.path.isfile(dataloc):
+    raise FileNotFoundError(f"file not found: {dataloc}")
 
-all_features, all_data = [], []
-try:
-    all_features, all_data = process_data.load_data(data_path=save_path,
-                                                    write_file=True,
-                                                    write_path=data_directory
-                                                    )
-    print(f"length of all data: {len(all_data)}")
-except Exception as e:
-    print(f"error when processing features: {e}")
-
-if all_features is None or all_data is None:
-    print("error - no data found")
-else:
-    training_data, testing_data = process_data.separate_sets(all_data)
-    y_cols = ["PRCP"]
-    Y = process_data.get_selected_features(data_path=os.path.join(data_directory, "USW00003184.csv"),
-                                           features_arr=y_cols,
-                                           write_file=True,
-                                           write_path=data_directory
-                                           )
-
+X, feature_names = get_data.parse_csv(dataloc, save_headers=True)
+print(feature_names[:20])
+print(X[ :20])
 
 
 
